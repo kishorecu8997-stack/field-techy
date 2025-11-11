7eec24c57e762f3447af54ab19c9b938faa5744e
karthik <santhoshkarthisk005@gmail.com>
2025-11-11 15:19:30 +0530

    code formatting

--- web/src/pages/admin/rate_card/index.tsx
+++ web/src/pages/admin/rate_card/index.tsx
@@ -31,7 +31,13 @@ import type { RateCardProps } from "./types";
 const ManageRateCards: React.FC = () => {
   const navigate = useNavigate();
   const columns: Column<RateCardProps>[] = [
-        { key: "id", label: "Request ID", renderCell: (row: RateCardProps) => <div className="whitespace-nowrap">{row.id}</div> },
+    {
+      key: "id",
+      label: "Request ID",
+      renderCell: (row: RateCardProps) => (
+        <div className="whitespace-nowrap">{row.id}</div>
+      ),
+    },
     { key: "skillSet", label: "Skill Set" },
     { key: "region", label: "Region" },
     { key: "location", label: "Location" },
