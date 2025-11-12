7db1bb9facf897289ff1ef3883e3f14cf420ac61
maheswari3008 <maheswariravi3008@gmail.com>
2025-11-12 18:56:28 +0530

    Add: cms ui

--- web/src/pages/admin/manage_cms/index.tsx
+++ web/src/pages/admin/manage_cms/index.tsx
@@ -1,7 +1,13 @@
 "use client";
+import AdminTabComponent from "@/shared/components/AdminTabComponent";
 import { Button } from "@/shared/components/commonUI/Buttons";
-import { useQuillEditor } from "@/shared/components/commonUI/text_editor";
-import { useState } from "react";
+import CMSLegal from "./Legal";
+import CMSPrivacyPolicy from "./CmsPrivacyPolicy";
+import Faq from "./Faq";
+import TermsAndCondition from "./TermsAndCondition";
+import CustomerPrivacyPolicy from "./CustomerPrivacyPolicy";
+import AboutUs from "./AboutUs";
+import DataDelectionPolicy from "./DataDelectionPolicy";
 
 /**
  * ManageCMS Component
@@ -18,16 +24,62 @@ import { useState } from "react";
  * @returns {JSX.Element} The rendered ManageCMS component.
  */
 export default function ManageCMS() {
-  const [value, setValue] = useState("<p>Hello world</p>");
-  const { containerRef } = useQuillEditor({ value, onChange: setValue });
+  const tabs = [
+    {
+      label: "Contact Support",
+      content: "contact",
+      hide: false,
+    },
+    {
+      label: "Legal",
+      content: <CMSLegal />,
+      hide: false,
+    },
+    {
+      label: "Privacy Policy",
+      content: <CMSPrivacyPolicy />,
+      hide: false,
+    },
+    {
+      label: "FAQ",
+      content: <Faq />,
+      hide: false,
+    },
+    {
+      label: "Terms And Condition",
+      content: <TermsAndCondition />,
+      hide: false,
+    },
+    {
+      label: "Customer Privacy Policy",
+      content: <CustomerPrivacyPolicy />,
+      hide: false,
+    },
+    {
+      label: "About Us",
+      content: <AboutUs />,
+      hide: false,
+    },
+    {
+      label: "Data Delection Policy",
+      content: <DataDelectionPolicy />,
+      hide: false,
+    },
+  ];
 
   return (
-    <div className="w-full h-full flex flex-col">
-      <div ref={containerRef} className="h-full w-full" />
-      <div className="w-full flex justify-end ">
-        <Button className="w-fit " onClick={() => alert(value)}>
-          Save
+    <div className="w-full h-full p-4">
+      <div className="flex justify-between">
+        <p className="mt-2 mb-6 font-semibold">Manage CMS</p>
+        <Button
+          type="submit"
+          className="w-fit bg-gradient-to-r bg-teal-900 text-white py-2 rounded-lg hover:opacity-90 transition"
+        >
+          Back
         </Button>
+      </div>
+      <div className="bg-white dark:bg-gray-700 rounded-lg p-4">
+        <AdminTabComponent tabs={tabs} defaultActiveTab="Contact Support" />
       </div>
     </div>
   );
