da15d8f88f7c6dee9056978186765b43c9fef0a6
karthik <santhoshkarthisk005@gmail.com>
2025-11-20 12:22:16 +0530

    changes in job card

--- web/src/pages/engineer/my_job/job_details_components/JobHeaderCard.tsx
+++ web/src/pages/engineer/my_job/job_details_components/JobHeaderCard.tsx
@@ -34,149 +34,151 @@ const JobHeaderCard: React.FC<JobHeaderCardProps> = ({
   const { setActiveKey, setISOpenSidebar } = useDrawerStore();
 
   return (
-    <div
-      className={`${
-        isSendProposal ? "text-gray-800 bg-yellow-50" : "bg-teal-800 text-white"
-      } p-5 rounded-xl shadow-md`}
-    >
-      <div className="flex justify-between items-center">
-        <h1 className="text-xl md:text-2xl font-bold">{title}</h1>
-        <span className="bg-gray-300 px-3 py-1.5 rounded-full text-sm font-medium text-gray-900">
-          {type === WORKING_TYPES.onsite ? "On Site" : "Remote"}
-        </span>
-      </div>
+    <>
+      <div
+        className={`${
+          isSendProposal ? "text-gray-800 bg-yellow-50" : "bg-teal-800"
+        } p-5 rounded-xl shadow-md`}
+      >
+        <div className="flex justify-between items-center text-white">
+          <h1 className="text-xl md:text-2xl font-bold">{title}</h1>
+          <span className="bg-gray-300 px-3 py-1.5 rounded-full text-sm font-medium text-gray-900">
+            {type === WORKING_TYPES.onsite ? "On Site" : "Remote"}
+          </span>
+        </div>
 
-      <div className="mt-3 flex flex-wrap gap-4 text-sm">
-        <span className="flex items-center gap-1">🕒 {duration}</span>
-        <span>Client: {client}</span>
-      </div>
+        <div className="mt-3 flex flex-wrap gap-4 text-sm text-white">
+          <span className="flex items-center gap-1 ">🕒 {duration}</span>
+          <span>Client: {client}</span>
+        </div>
 
-      <div className="mt-4 flex flex-wrap gap-3 justify-end">
-        <span className="flex rounded-full text-sm font-medium">
-          {status === JOB_STATUSES.inprogress ? (
-            <div className="flex gap-2">
-              <Button
-                className="bg-teal-800 text-white px-6 py-2 rounded-full"
-                onClick={() => setOpen(true)}
-              >
-                Update Log
-              </Button>
-              <Button
-                className="bg-teal-800 text-white px-6 py-2 rounded-full"
-                onClick={() => setIsWorkSubmitted?.(true)}
-              >
-                Submit work
-              </Button>
-            </div>
-          ) : status === JOB_STATUSES.applied ? (
-            <div className="flex gap-2 items-center">
-              <icons.checkCircle className="text-green-500 w-6 h-6" />
-              <span className="text-lg">Job Applied</span>
-            </div>
-          ) : status === JOB_STATUSES.new ? (
-            <div className="flex gap-2 items-center">
-              {!isSendProposal && (
-                <>
-                  <Button
-                    className="bg-teal-800 text-white px-6 py-2 rounded-md"
-                    onClick={() => setSendProposal?.(true)}
-                  >
-                    Update Log
-                  </Button>
-                  <Button
-                    className="bg-teal-800 text-white px-6 py-2 rounded-md font-medium border border-gray-300"
-                    onClick={() => {
-                      setIsWorkSubmitted?.(true);
-                      setActiveTab("Work Submissions");
-                    }}
-                  >
-                    View Job posting
-                  </Button>
-                </>
-              )}
-            </div>
-          ) : status === JOB_STATUSES.offer ? (
-            <div className="flex gap-2 items-center">
-              {OfferJobStatus === undefined ? (
-                <div className="flex gap-4">
-                  <Button
-                    className="bg-teal-800 text-white px-6 py-2 rounded-md"
-                    onClick={() => setOfferJobStatus("accepted")}
-                  >
-                    Accept Job
-                  </Button>
-
-                  <Button
-                    className="bg-teal-800 text-white px-6 py-2 rounded-md"
-                    onClick={() => {
-                      setOfferJobStatus("declined");
-                      setActiveKey("cancelOffer");
-                      setISOpenSidebar(true);
-                    }}
-                  >
-                    Decline
-                  </Button>
-                </div>
-              ) : OfferJobStatus === "accepted" ? (
-                <div className="flex gap-4">
-                  <Button
-                    className="bg-teal-800 text-white px-6 py-2 rounded-md"
-                    onClick={() => setOfferJobStatus("started")}
-                  >
-                    Start Job
-                  </Button>
-
-                  <Button
-                    className="bg-teal-800 text-white px-6 py-2 rounded-md"
-                    onClick={() => {
-                      setOfferJobStatus("declined");
-                      setActiveKey("cancelOffer");
-                      setISOpenSidebar(true);
-                    }}
-                  >
-                    Decline
-                  </Button>
-                </div>
-              ) : OfferJobStatus === "started" ? (
+        <div className="mt-4 flex flex-wrap gap-3 justify-end">
+          <span className="flex rounded-full text-sm font-medium">
+            {status === JOB_STATUSES.inprogress ? (
+              <div className="flex gap-2">
                 <Button
-                  className="bg-teal-800 text-white px-6 py-2 rounded-md"
-                  onClick={() => setOfferJobStatus("checked-in")}
+                  variant="primary"
+                  className=" rounded-md"
+                  onClick={() => setOpen(true)}
                 >
-                  Check in
+                  Update Log
                 </Button>
-              ) : (
-                <div className="flex gap-2">
-                  <Button
-                    className="bg-teal-800 text-white px-6 py-2 rounded-md"
-                    onClick={() => setOpen(true)}
-                  >
-                    Update Log
-                  </Button>
-                  <Button
-                    className="bg-teal-800 text-white px-6 py-2 rounded-md"
-                    onClick={() => {
-                      setIsWorkSubmitted?.(true);
-                      setActiveTab("Work Submissions");
-                    }}
-                  >
-                    Submit Work
-                  </Button>
-                </div>
-              )}
-            </div>
-          ) : (
-            <div className="flex gap-2 items-center">
-              <icons.checkCircle className="text-green-500 w-6 h-6" />
-              <span className="text-lg">Job Completed</span>
-            </div>
-          )}
-        </span>
-      </div>
+                <Button
+                   variant="secondary"
+                  onClick={() => setIsWorkSubmitted?.(true)}
+                >
+                  Submit work
+                </Button>
+              </div>
+            ) : status === JOB_STATUSES.applied ? (
+              <div className="flex gap-2 items-center">
+                <icons.checkCircle className="text-green-500 w-6 h-6" />
+                <span className="text-lg text-white">Job Applied</span>
+              </div>
+            ) : status === JOB_STATUSES.new ? (
+              <div className="flex gap-2 items-center">
+                {!isSendProposal && (
+                  <>
+                    <Button
+                      className="bg-teal-800 text-white px-6 py-2 rounded-md"
+                      onClick={() => setSendProposal?.(true)}
+                    >
+                      Update Log
+                    </Button>
+                    <Button
+                      className="bg-teal-800 text-white px-6 py-2 rounded-md font-medium border border-gray-300"
+                      onClick={() => {
+                        setIsWorkSubmitted?.(true);
+                        setActiveTab("Work Submissions");
+                      }}
+                    >
+                      View Job posting
+                    </Button>
+                  </>
+                )}
+              </div>
+            ) : status === JOB_STATUSES.offer ? (
+              <div className="flex gap-2 items-center">
+                {OfferJobStatus === undefined ? (
+                  <div className="flex gap-4">
+                    <Button
+                     variant="primary"
+                      onClick={() => setOfferJobStatus("accepted")}
+                    >
+                      Accept Job
+                    </Button>
 
+                    <Button
+                     variant="secondary"
+                      onClick={() => {
+                        setOfferJobStatus("declined");
+                        setActiveKey("cancelOffer");
+                        setISOpenSidebar(true);
+                      }}
+                    >
+                      Decline
+                    </Button>
+                  </div>
+                ) : OfferJobStatus === "accepted" ? (
+                  <div className="flex gap-4">
+                    <Button
+                      variant="primary"
+                      onClick={() => setOfferJobStatus("started")}
+                    >
+                      Start Job
+                    </Button>
+
+                    <Button
+                      variant="secondary"
+                      onClick={() => {
+                        setOfferJobStatus("declined");
+                        setActiveKey("cancelOffer");
+                        setISOpenSidebar(true);
+                      }}
+                    >
+                      Decline
+                    </Button>
+                  </div>
+                ) : OfferJobStatus === "started" ? (
+                  <Button
+                    variant="primary"
+                    onClick={() => setOfferJobStatus("checked-in")}
+                  >
+                    Check in
+                  </Button>
+                ) : (
+                  <div className="flex gap-2">
+                    <Button
+                       variant="primary"
+                      onClick={() => setOpen(true)}
+                    >
+                      Update Log
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      onClick={() => {
+                        setIsWorkSubmitted?.(true);
+                        setActiveTab("Work Submissions");
+                      }}
+                    >
+                      Submit Work
+                    </Button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div className="flex gap-2 items-center">
+                <icons.checkCircle className="text-green-500 w-6 h-6" />
+                <span className="text-lg text-white">Job Completed</span>
+              </div>
+            )}
+          </span>
+        </div>
+      </div>
       <Popup open={open} onClose={() => setOpen(false)}>
         <UpdateStatus onClose={() => setOpen(false)} />
       </Popup>
-    </div>
+    </>
   );
 };
 
