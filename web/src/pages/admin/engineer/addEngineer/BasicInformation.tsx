c69bd1c1c03f5c012363da2343d118ff46eda44e
Apiramy <apiramy.jeyaharan@teceze.com>
2025-12-05 18:28:28 +0530

    Admin page numeric only input

--- web/src/pages/admin/engineer/addEngineer/BasicInformation.tsx
+++ web/src/pages/admin/engineer/addEngineer/BasicInformation.tsx
@@ -105,8 +105,8 @@ export default function BasicInformation() {
             type="text"
             placeholder="Enter Price per/hour"
             required
+            allowedCharacters="currency"
             rules={{ validate: (v: string) => validatePricePerHour(v) }}
-          
           />
         </div>
       </div>
