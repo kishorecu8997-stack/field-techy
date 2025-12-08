3139d68df71faf86d4a0af64a2f315ef28bb9653
Apiramy <apiramy.jeyaharan@teceze.com>
2025-12-08 13:28:57 +0530

    reviewd update

--- web/src/shared/components/commonUI/inputs/InputField.tsx
+++ web/src/shared/components/commonUI/inputs/InputField.tsx
@@ -18,6 +18,7 @@ interface InputFieldProps {
   inputClassName?: string;
   showValidationCheck?: boolean;
   disabled?: boolean;
+  inputMode?: "number" | "string" | "both";
   onChange?: (value: string) => void;
   allowedCharacters?:
     | "numbers"
@@ -39,6 +40,7 @@ export const InputField = ({
   containerClassName = "flex flex-col py-1 w-full",
   inputClassName = "w-full rounded-md border border-gray-300 dark:border-gray-600 py-3 px-5  text-base text-gray-900 dark:text-gray-100 placeholder-gray-400 dark:placeholder-gray-500 outline-none focus:ring-2 focus:ring-primary transition",
   showValidationCheck = false,
+  inputMode = "both", // default
   disabled = false,
   onChange,
   allowedCharacters,
