2cc44f259836a19766ea08a8e86587a5272234a6
Apiramy <apiramy.jeyaharan@teceze.com>
2025-12-11 11:02:53 +0530

    test

--- web/src/shared/components/AddCard.tsx
+++ web/src/shared/components/AddCard.tsx
@@ -66,7 +66,7 @@ const AddCard: React.FC<AddCardProps> = ({ onClose, onAddCard }) => {
     if (isValid) {
       const data = methods.getValues();
       console.log("Valid card data:", data);
-      toast.success("Card added successfully.");
+      toast.success("Card added successfullyy.");
       onClose();
       onAddCard(data);
     }
