3d2adf83d679977388c3a0ec8337b0bbe4213fb6
b-sathusdikan <sathusdikan.baskaran@teceze.com>
2025-12-26 14:08:29 +0530

    i fixed

--- web/src/shared/components/jobSearchBarClient.tsx
+++ web/src/shared/components/jobSearchBarClient.tsx
@@ -4,7 +4,6 @@ import { FaMapMarkerAlt, FaSearch } from "react-icons/fa"; // or use your own ic
 import { useNavigate } from "react-router-dom";
 import { InputField } from "./commonUI/inputs";
 import { FormContainer } from "./commonUI/inputs/FormContainer";
-import { useEffect } from "react";
 
 /**
  * JobSearchBar component provides a dual-input search form for jobs and location.
@@ -21,7 +20,7 @@ export const JobSearchBarClient = () => {
 
   return (
     <FormContainer
-      onSubmit={() => { }}
+      onSubmit={() => {}}
       methods={methods}
       className="flex items-center w-full max-w-xl mx-auto bg-gray-100 dark:bg-gray-700 rounded-full overflow-hidden shadow-sm z-0"
     >
@@ -31,21 +30,9 @@ export const JobSearchBarClient = () => {
           placeholder="Search Jobs.."
           leftIcon={<FaSearch className="text-gray-400" />}
           containerClassName="flex-1 py-0"
+          onChange={() => navigate(absoluteUrls.client.home.search_result)}
           inputClassName="border-none bg-transparent rounded-none text-gray-900 dark:text-gray-100 pr-3 focus:outline-none py-2"
-          rules={{
-            onChange: (e) => {
-              const raw = e.target.value;
-              const sanitized = raw.replace(/[^a-zA-Z0-9]/g, "");
-
-              // navigate on input change
-              navigate(absoluteUrls.client.home.search_result);
-
-              return sanitized;
-            }
-          }}
         />
-
-
         <div className="h-8 w-px bg-gray-300 dark:bg-gray-600"></div>
         <InputField
           name="location"
