a29bb32d31755d7709d0047ad71383a211c244bb
Mathiyaseelan Ruckshanth <Ruckshanth.Mathiyaseelan@teceze.com>
2025-12-31 10:30:39 +0530

    onboarding data

---
+++ web/src/dummy_data/onBoardingDate.ts
@@ -0,0 +1,49 @@
+ interface stepsProps {
+  selector: string;
+  content: string;
+  title: string;
+}
+
+export const steps: stepsProps[] = [
+  {
+    selector: "#welcome",
+    title: "Welcome ",
+    content:
+      "✨ “Welcome aboard — every great story starts with a single visit.”",
+  },
+  {
+    selector: "#exploreJobs",
+    title: "ExploreJobs",
+    content:
+      "Explore Jobs Browse available jobs and find work that matches your skills.",
+  },
+  {
+    selector: "#featuredJobs",
+    title: "FeaturedJobs",
+    content:
+      "Featured Jobs Quickly apply to highlighted jobs with high demand.",
+  },
+  {
+    selector: "#recommendedJobs",
+    title: "RecommendedJobs",
+    content:
+      "Recommended Jobs Jobs suggested based on your profile and activity.",
+  },
+  {
+    selector: "#myEarnings",
+    title: "MyEarnings",
+    content: "My Earnings Skip Track your total earnings from completed jobs.",
+  },
+  {
+    selector: "#withdrawMoney",
+    title: "WithdrawMoney",
+    content:
+      "Withdraw Money Skip Transfer your available balance to your bank account.",
+  },
+  {
+    selector: "#completeProfile",
+    title: "CompleteProfile",
+    content:
+      "Complete Your Profile Skip A higher profile score helps you get more jobs.",
+  },
+];
