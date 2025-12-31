02078bc0d9bd75dd1c14a624dae913464f48b688
Mathiyaseelan Ruckshanth <Ruckshanth.Mathiyaseelan@teceze.com>
2025-12-31 10:30:27 +0530

    Main Tour provider

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -3,7 +3,11 @@ import { routes } from "./routes/AppRoute";
 import OfflineBanner from "@/shared/components/commonUI/OfflineBanner";
 import { useOfflineSync } from "@/offline/useOfflineSync";
 import { useJobExpirationNotification } from "@/hooks/useJobExpirationNotifications";
-
+import { steps } from "@/dummy_data/onBoardingDate";
+import { TourProvider } from "@reactour/tour";
+import OnboardingFlowGuide, {
+  tourStyles,
+} from "@/pages/engineer/home/components/OnboardingFlowGuide";
 
 /**
  * The main application component that sets up routing.
@@ -14,15 +18,22 @@ import { useJobExpirationNotification } from "@/hooks/useJobExpirationNotificati
  * @component
  * @returns {JSX.Element} The application with routing enabled.
  */
-
 const App = () => {
   useOfflineSync();
   useJobExpirationNotification();
-  return(
+  return (
     <>
-      <OfflineBanner />
-     <RouterProvider router={routes} /> 
-     </>
-  ) 
+      <TourProvider
+        className="rounded-2xl"
+        steps={steps}
+        styles={tourStyles}
+        ContentComponent={OnboardingFlowGuide}
+        defaultOpen
+      >
+        <OfflineBanner />
+        <RouterProvider router={routes} />
+      </TourProvider>
+    </>
+  );
 };
 export default App;
