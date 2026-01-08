9f615b4cd2efa714b1a7d926eb19f9d7f5aa1c77
b-sathusdikan <sathusdikan.baskaran@teceze.com>
2026-01-08 15:40:11 +0530

    357-Engineer - Break Details not visible in Dark Mode

--- web/src/pages/engineer/my_job/job_details_components/jobHeaderComponents/BreakDetails.tsx
+++ web/src/pages/engineer/my_job/job_details_components/jobHeaderComponents/BreakDetails.tsx
@@ -15,8 +15,8 @@ import BreakCalendar from "./BreakCalendar";
  */
 const BreakDetails: React.FC = () => {
   return (
-    <div className="py-6 px-4 max-w-full bg-white">
-      <h2 className="text-2xl font-bold mb-6">Break Details</h2>
+    <div className="py-6 px-4 max-w-full bg-white dark:bg-gray-900">
+      <h2 className="text-2xl font-bold mb-6 ">Break Details</h2>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         <div className="order-2 lg:order-1 flex flex-col">
           <h3 className="text-xl font-semibold mb-4">Break Status</h3>
@@ -27,7 +27,9 @@ const BreakDetails: React.FC = () => {
           </div>
         </div>
         <div className="order-1 lg:order-2 flex flex-col">
-          <h3 className="text-xl font-semibold mb-4">Calendar view of Breaks</h3>
+          <h3 className="text-xl font-semibold mb-4">
+            Calendar view of Breaks
+          </h3>
           <div className="bg-white rounded-xl shadow-md overflow-hidden flex-1 min-h-0">
             <div className="h-full">
               <BreakCalendar />
