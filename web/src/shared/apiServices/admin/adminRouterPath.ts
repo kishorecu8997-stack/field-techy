1f91a4df0852d4748d926ae13ec9fc676d2a2d58
MRTitus02 <titus.senthilkumaran@teceze.com>
2026-01-08 12:46:28 +0530

    modified admin routes

--- web/src/shared/apiServices/admin/adminRouterPath.ts
+++ web/src/shared/apiServices/admin/adminRouterPath.ts
@@ -5,7 +5,7 @@ export const ADMIN_ROUTER_PATHS = {
     `/user/api/v1/users/otp/request/${phoneOrEmail}`,
   RESET_PASSWORD_USING_OTP: (otp: string) =>
     `/user/api/v1/users/password/reset/by-otp/${otp}`,
-    CREATE_NOTIFICATION: "/api/v1/notification/create",
+    CREATE_NOTIFICATION: "/admin/api/v1/admin/notification/create",
     GET_NOTIFICATION_BY_ID: (id: string) => `/admin/api/v1/admin/notification/${id}`,
-    EDIT_NOTIFICATION: (id: string) => `/api/v1/admin/notification/edit/${id}`,
+    EDIT_NOTIFICATION: (id: string) => `/admin/api/v1/admin/notification/edit/${id}`,
 } as const;
