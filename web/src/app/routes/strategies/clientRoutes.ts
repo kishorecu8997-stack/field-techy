e2dd66c16b6ea10c5e8786fb0985ad63d3cb0e1e
karthik <santhoshkarthisk005@gmail.com>
2026-01-09 11:34:43 +0530

    changes for map overflow changes

--- web/src/app/routes/strategies/clientRoutes.ts
+++ web/src/app/routes/strategies/clientRoutes.ts
@@ -85,7 +85,6 @@ export class ClientRouteStrategy implements RouteStrategy {
             withSuspense(Components.LiveChatWidget),
           ],
         }),
-        // element: withSuspense(Components.ClientLayout),
         children: [
           { index: true, element: withSuspense(Components.ClientMyJobsPage) },
           {
