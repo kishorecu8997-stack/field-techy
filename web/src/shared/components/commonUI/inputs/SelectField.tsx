2b84c2aac9c772341cbb167a9779974b101dec04
PavithiranTeceze <Pavithiran.Thevarasa@teceze.com>
2026-01-21 08:01:48 +0530

    Dark theme support - scrollbars

--- web/src/shared/components/commonUI/inputs/SelectField.tsx
+++ web/src/shared/components/commonUI/inputs/SelectField.tsx
@@ -219,7 +219,7 @@ export const SelectField = ({
                         )}
                         <span
                           className={`block truncate w-full ${
-                            !value ? "text-gray-400" : ""
+                            !value ? "text-gray-400 dark:text-gray-500" : "text-gray-900 dark:text-white"
                           }`}
                         >
                           {displayLabel}
@@ -291,7 +291,7 @@ export const SelectField = ({
                                       />
                                     )}
                                     <span
-                                      className={`block truncate ${
+                                      className={`block truncate text-gray-900 dark:text-white ${
                                         selected
                                           ? "font-semibold"
                                           : "font-normal"
