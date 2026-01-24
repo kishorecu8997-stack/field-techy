4006354edfcf64dc58ea3d58ccd99e92b8113648
kurichi0323 <kalathevan.kurinchinilavan@teceze.com>
2026-01-24 17:20:21 +0530

    build-and-run

--- web/src/pages/client/post_job/PostAJobComponent/components/PostAJobFields.tsx
+++ web/src/pages/client/post_job/PostAJobComponent/components/PostAJobFields.tsx
@@ -130,7 +130,10 @@ const PostAJobFields = ({
     ...section,
     items: section.items.map((item) => ({
       ...item,
-      value: item.value instanceof Date ? item.value.toISOString() : item.value,
+      value:
+        item.value && typeof item.value === "object" && "getTime" in item.value
+          ? (item.value as Date).toISOString()
+          : item.value,
     })),
   }));
 
