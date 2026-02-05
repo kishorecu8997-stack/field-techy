680440f10573181ecaad9b37f2bd5f48ed376d24
kurichi0323 <kalathevan.kurinchinilavan@teceze.com>
2026-02-05 14:37:02 +0530

    Side bar button's scroll up issues fixed

--- web/src/pages/client/privacy_policy/FAQ.tsx
+++ web/src/pages/client/privacy_policy/FAQ.tsx
@@ -1,12 +1,17 @@
 import { clientFAQData } from "@/dummy_data/clientPolicyDatas";
 import MyJobsHeader from "@/shared/components/MyJobsHeader";
 import ContentPage from "./ContentPage";
+import { scrollToTop } from "@/utils";
+import { useEffect } from "react";
 
 /**
  * FAQ page displaying frequently asked questions using static dummy data.
  * Renders a header and content section via reusable components.
  */
 const FAQ = () => {
+   useEffect(() => {
+    scrollToTop();
+  }, []);
   return (
     <div className="min-h-[45rem] bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       <div className="container mx-auto px-4 py-6 md:px-6">
