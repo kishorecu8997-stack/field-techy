880c58e46d4e4139f813f708705a427d7c91f86b
Aatharsan-Bala <aatharsan.balachandran@teceze.com>
2026-02-16 16:54:06 +0530

    maheswari review comments has been addressed

--- web/src/pages/client/auth/components/ForgetPassword.tsx
+++ web/src/pages/client/auth/components/ForgetPassword.tsx
@@ -1,7 +1,18 @@
 import AuthForgetPassword from "@/shared/components/auth/AuthForgetPassword";
+import { UserRole } from "@/shared/enums/users";
+/**
+ * ForgetPassword component for client authentication.
+ *
+ * This component renders the AuthForgetPassword component
+ * with the "client" role prop, providing the "Forgot Password"
+ * functionality for client users.
+ *
+ * @component
+ * @returns {JSX.Element} The rendered AuthForgetPassword component for clients.
+ */
 
 const ForgetPassword = () => {
-  return <AuthForgetPassword role="client" />;
+  return <AuthForgetPassword role={UserRole.CLIENT} />;
 };
 
 export default ForgetPassword;
