55bb92db88fe2a0a4f345a52aa8ccf140785f5d4
kurichi0323 <kalathevan.kurinchinilavan@teceze.com>
2026-02-24 14:56:33 +0530

    format

--- web/src/pages/client/my_account/ClientAccountDrawerMenu.tsx
+++ web/src/pages/client/my_account/ClientAccountDrawerMenu.tsx
@@ -132,7 +132,7 @@ const ClientAccountDrawerMenu: React.FC<ClientDrawerMenuProps> = ({
 
   const menuItems: ClientMenuItems[] = [
     //This is commented out because the Manage Proposal feature is planned to be included under the "My Jobs" section.
-    // { 
+    // {
     //   label: "Manage Proposal",
     //   icon: IoDocumentText,
     //   key: "proposal",
