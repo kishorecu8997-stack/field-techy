b5f60f8637448bcedfae59d0bc8f19806ff76734
Mathiyaseelan Ruckshanth <Ruckshanth.Mathiyaseelan@teceze.com>
2026-03-03 18:29:29 +0530

    resolved at fixes

--- web/src/pages/client/job_details/components/ReportDetails.tsx
+++ web/src/pages/client/job_details/components/ReportDetails.tsx
@@ -96,7 +96,7 @@ const ReportDetails: React.FC = () => {
       time: "---",
     };
 
-    const resolvedInfo = formatApiDateTime(data.resolveAt) ?? {
+    const resolvedInfo = formatApiDateTime(data.resolvedAt) ?? {
       date: "---",
       time: "---",
     };
@@ -122,7 +122,7 @@ const ReportDetails: React.FC = () => {
         time: resolvedInfo.time,
         color:
           data.status === "resolved" ? "border-emerald-600" : "border-gray-300",
-        isActive: !!data.resolveAt,
+        isActive: !!data.resolvedAt,
       },
     ];
 
