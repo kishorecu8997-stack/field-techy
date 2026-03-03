8917b5b689419790221e18dbf8cb0005872f85c9
karthik <santhoshkarthisk005@gmail.com>
2026-03-03 11:01:46 +0530

    - Added the global error boundary. - Updated the NotFound page.

--- web/src/shared/components/NotFound.tsx
+++ web/src/shared/components/NotFound.tsx
@@ -1,22 +1,29 @@
-import { absoluteUrls } from "@/config/urls";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Button } from "./commonUI/Buttons";
 
 /**
  * 404 Not Found page with a link back to the home dashboard.
  */
 const NotFound = () => {
+
+  const navigate = useNavigate();
+
+  const handleGoToHome = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh] p-4 text-center">
       <h1 className="text-4xl font-bold text-gray-800 mb-4">404</h1>
       <p className="text-lg text-gray-600 mb-6">
         Oops! The page you're looking for doesn't exist.
       </p>
-      <Link
-        to={absoluteUrls.engineer.home.my_jobs}
+      <Button
+        onClick={handleGoToHome}
         className="px-4 py-2 bg-emerald-600 text-white rounded-md hover:bg-emerald-700 transition"
       >
-        Go to Home
-      </Link>
+        Go Back
+      </Button>
     </div>
   );
 };
