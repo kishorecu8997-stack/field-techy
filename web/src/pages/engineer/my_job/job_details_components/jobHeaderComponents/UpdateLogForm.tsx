2549fe26a943436421e766968b2b2f84b53b08ad
keerthigan-teceze <Keerthigan.Thevarasa@teceze.com>
2026-03-06 12:36:19 +0530

    fix build error

--- web/src/pages/engineer/my_job/job_details_components/jobHeaderComponents/UpdateLogForm.tsx
+++ web/src/pages/engineer/my_job/job_details_components/jobHeaderComponents/UpdateLogForm.tsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
 import { Button } from "@/shared/components/commonUI/Buttons";
-import { InputField, TextareaInput } from "@/shared/components/commonUI/inputs";
+import { TextareaInput } from "@/shared/components/commonUI/inputs";
 import { FileUpload } from "@/shared/components/commonUI/inputs/FileUpload";
 import { FormContainer } from "@/shared/components/commonUI/inputs/FormContainer";
 import { usePopupStore } from "@/shared/store/popupStore";
