ca7f3adcc43d70bbae1646ff1ce200c242afc22f
Apiramy <apiramy.jeyaharan@teceze.com>
2026-03-06 14:02:26 +0530

    mapping file added

---
+++ web/src/shared/constants/transactionMaps.ts
@@ -0,0 +1,15 @@
+export const paymentStatusMap: Record<string, string> = {
+  approved: "Approved",
+  pending: "Pending",
+  rejected: "Rejected",
+};
+
+export const transactionTypeMap: Record<string, string> = {
+  debit: "Debit",
+  credit: "Credit",
+};
+
+export const categoryMap: Record<string, string> = {
+  jobrelated: "Job Related",
+  bankrelated: "Bank Related",
+};
