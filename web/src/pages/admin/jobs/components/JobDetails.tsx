7e0ea30fe9ca506484ea75e710e81de318d2b1e0
Aatharsan-Bala <aatharsan.balachandran@teceze.com>
2026-03-16 17:00:11 +0530

    `Update condition in JobDetails component to include "submitted" status`

--- web/src/pages/admin/jobs/components/JobDetails.tsx
+++ web/src/pages/admin/jobs/components/JobDetails.tsx
@@ -56,7 +56,8 @@ const JobDetails = () => {
     (engineer) =>
       engineer.assignmentStatus === "started" ||
       engineer.assignmentStatus === "assigned" ||
-      engineer.assignmentStatus === "paid",
+      engineer.assignmentStatus === "paid"||
+      engineer.assignmentStatus === "submitted",
   );
 
   const showJobDescriptionModal = (description?: string) => {
